019578cb4908d8e6f853ef96c4f4f8e8326dc615
Shn Kerby Dolamos <kerbydolamoscs@gmail.com>
2025-03-17 19:01:00 +0800

    [Exceptions] Added new Exception
    Exception that handles when a book is not found in list of book in a
    member.

---
+++ Exceptions/BooknotFoundException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library_System.Classes;
+
+namespace Library_System.Exceptions
+{
+    public class BooknotFoundException : Exception
+    {
+        public Book Borrowed_Book { get; private set; }
+        public BooknotFoundException(string message, Book borrowed_book) : base(message)
+        {
+            this.Borrowed_Book = borrowed_book;
+        }
+    }
+}

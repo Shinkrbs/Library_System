71615c848e8f1a5d2e0822a41cb4531a50a007aa
Shn Kerby Dolamos <kerbydolamoscs@gmail.com>
2025-03-17 18:53:10 +0800

    [Exceptions] Added new Exception
    Exception that handles when a book is not available for borrowing.

---
+++ Exceptions/BooknotAvailableException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library_System.Classes;
+
+namespace Library_System.Exceptions
+{
+    public class BooknotAvailableException : Exception
+    {
+        public Book Book_Borrowed { get; private set; }
+        public BooknotAvailableException(string message, Book book_borrowed) : base(message)
+        {
+            this.Book_Borrowed = book_borrowed;
+        }
+    }
+}
